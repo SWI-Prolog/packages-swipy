14a3ae6c53288a2a633feb95d0e54fbcb9c21752
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-07-25 17:37:51 +0200

    Updated benchmarks

--- tests/bench.pl
+++ tests/bench.pl
@@ -6,20 +6,33 @@ here.
     file_directory_name(File, Dir),
     add_py_lib_dir(Dir, first).
 
+bench(N) :-
+    bench_py_call(N),
+    bench_iter(N, _),
+    bench_call_prolog(N),
+    bench_echo_list(N),
+    bench_pass_list(N).
+
 bench_py_call(N) :-
+    ansi_format(bold, 'Calling python function ~D times~n', [N]),
     time(forall(between(1,N,_I),
-                py_call(demo:sumlist3(5,[1,2,3]), _L))).
+                py_call(demo:int(), _L))).
 
 bench_iter(N, Sum) :-
+    ansi_format(bold, 'Iterate over Prolog goal with ~D answers~n', [N]),
     time(py_call(demo:bench_iter(N), Sum)).
 
 bench_call_prolog(N) :-
+    ansi_format(bold, 'Call Prolog predicate from Python ~D times~n', [N]),
     time(py_call(demo:bench_call(N), _)).
 
+bench_pass_list(N) :-
+    ansi_format(bold, 'Pass list with ~D integers to Python~n', [N]),
+    numlist(1, N, L),
+    time(py_call(demo:echo(L))).
+
 bench_echo_list(N) :-
+    ansi_format(bold, 'Echo list with ~D integers to Python~n', [N]),
     numlist(1, N, L),
     time(py_call(demo:echo(L), _)).
 
-bench_pass_list(N) :-
-    numlist(1, N, L),
-    time(py_call(demo:echo(L))).
--- tests/demo.py
+++ tests/demo.py
@@ -20,18 +20,17 @@ def multiply(a,b):
 def concat(a,b):
     return a+b;
 
-def trivial(val):
-    return val;
-
-def trivial_v(val):
-    print(val);
-    return val;
-
 def tuple():
     return (1,2)
 
 def kwd(a, b, c=3):
     return {"a":a, "b":b, "c":c}
+
+def sumlist3(X,Y):
+    Z = []
+    for element in Y:
+        Z.append(X+element)
+    return Z
 
 # Demo for dealing with iterators
 
