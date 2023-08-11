542d042596f88bc0d623283955524ca0cf950770
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-08-11 14:30:45 +0200

    Added licence conditions
    After permission from the authors.

--- xsb_tests/Person.py
+++ xsb_tests/Person.py
@@ -1,3 +1,24 @@
+# Copyright 2023 Theresa Swift and Carl Anderson
+#
+# Permission is hereby granted, free of charge,  to any person obtaining a
+# copy  of  this  software  and    associated   documentation  files  (the
+# “Software”), to deal in  the   Software  without  restriction, including
+# without limitation the rights to  use,   copy,  modify,  merge, publish,
+# distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+# permit persons to whom the Software is   furnished  to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this  permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+# OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 class Person:
   def __init__(self, name, age, favorite_ice_cream=None):
     self.name = name
@@ -16,13 +37,13 @@ class Person:
     return("Hello my name is " + self.name + " and I'm a " + myclass1 + " " + myclass2)
 
   def func3(self,myclass1,myclass2,myclass3):
-    return("Hello my name is " + self.name + " and I'm a " + myclass1 + " " + myclass2 
+    return("Hello my name is " + self.name + " and I'm a " + myclass1 + " " + myclass2
            + " " + myclass3)
 
   def func4(self,myclass1,myclass2,myclass3,myclas4):
-    return("Hello my name is " + self.name + " and I'm a " + myclass1 + " " + myclass2 
+    return("Hello my name is " + self.name + " and I'm a " + myclass1 + " " + myclass2
            + " " + myclass3 + " I mean " + mylass4)
 
-  
+
 #p1 = Person("John", 36)
-#p1.myfunc() 
+#p1.myfunc()
--- xsb_tests/jintf.py
+++ xsb_tests/jintf.py
@@ -1,30 +1,36 @@
+# Copyright 2023 Theresa Swift and Carl Anderson
+#
+# Permission is hereby granted, free of charge,  to any person obtaining a
+# copy  of  this  software  and    associated   documentation  files  (the
+# “Software”), to deal in  the   Software  without  restriction, including
+# without limitation the rights to  use,   copy,  modify,  merge, publish,
+# distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+# permit persons to whom the Software is   furnished  to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this  permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+# OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#from json import *
 import json
-
-#def prolog_loads(String):
-#    jdict = json.loads(String)
-#    jlist = list(jdict.items())
-#    return(jlist)
 
 def prolog_loads(String):
     jdict = json.loads(String)
-#    jlist = list(jdict.items())
     return(jdict)
-
-#def prolog_load(File):
-#    with open(File) as fileptr:
-#        data = json.load(fileptr)
-#        jlist = dict_to_list(data)
-#        print(jlist)
-#        return(jlist)
 
 def prolog_load(File):
     with open(File) as fileptr:
         data = json.load(fileptr)
         return(data)
 
-#should not be needed -- transformation now done in C.    
+#should not be needed -- transformation now done in C.
 def dict_to_list(indict):
     originally_dict = False
     orig_struct = indict
@@ -37,9 +43,7 @@ def dict_to_list(indict):
     print("  ",end = " ")
     print(indict)
     for elt in indict:
-#        print("    ",end = " ")
-#        print(elt)
-        if type(elt) in [dict,list,tuple]: 
+        if type(elt) in [dict,list,tuple]:
             newelt = dict_to_list(elt)
         else:
             newelt = elt
@@ -50,7 +54,6 @@ def dict_to_list(indict):
         retstruct = ("__dict",retstruct)
     return(retstruct)
 
-            
 # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
 
 def prolog_dumps(list):
