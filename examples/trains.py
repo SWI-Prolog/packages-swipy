fa7b718ea3d446ef734219b4a57caaeb49ed5148
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-08-27 13:37:12 +0200

    Small demo

---
+++ examples/trains.py
@@ -0,0 +1,8 @@
+import janus_swi as janus
+
+janus.consult("trains","""
+train('Amsterdam', 'Haarlem').
+train('Amsterdam', 'Schiphol').
+""")
+
+print([d['Tuple'] for d in janus.Query("train(_From,_To),Tuple=_From-_To")])
