d2bebae07c69deb36addf2b1fbf63734235220dc
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-09-19 10:10:45 +0200

    Bump version.  Getting fairly mature.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ elif ( sys.platform == 'win32' ):
     PLLIB="libswipl"
 
 setup(name='janus_swi',
-      version='0.8.0',
+      version='0.9.0',
       description="Janus library to call SWI-Prolog",
       author="Jan Wielemaker",
       author_email="jan@swi-prolog.org",
