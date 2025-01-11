26f2df7c045788c68127c7f23e87f75f30c322ff
Jan Wielemaker <jan@swi-prolog.org>
2025-01-11 14:09:30 +0100

    Preparing version 1.5.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif ( sys.platform == 'win32' ):
     PLLIB="libswipl"
 
 setup(name='janus_swi',
-      version='1.5.1',
+      version='1.5.2',
       description="Janus library to call SWI-Prolog",
       author="Jan Wielemaker",
       author_email="jan@swi-prolog.org",
